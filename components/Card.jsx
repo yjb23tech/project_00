c8a6f8b575a5089fddac27f11637a99acbee659c
yjb23tech <yjb23tech@gmail.com>
2024-02-05 00:30:14 +0000

    Stay Up

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,23 +1,23 @@
-import album_icon from "./public/images/album_icon.png"
-import artist_icon from "./public/images/artist_icon.png"
-import genre_icon from "./public/images/genre_icon.png"
+import album_icon from "../public/images/album_icon.png"
+import artist_icon from "../public/images/artist_icon.png"
+import genre_icon from "../public/images/genre_icon.png"
 
 const Card = (props) => {
 
     return (
         <div className="song-card">
-            <img src={`images/${props.song_cover_art}`} className="song-card-cover-art"/>
-            <div className="song-card-contents">
+            <img src={`images/${props.song_cover_art}`} className="util-box-border-red song-card-cover-art"/>
+            <div className=" util-box-border-blue song-card-contents">
                 <h2>{props.song_title}</h2>
-                <div className="song-info-group">
+                <div className="util-box-border-green song-info-group">
                     <img src={album_icon} />
                     <p>{props.song_album_title}</p>
                 </div>
-                <div className="song-info-group">
+                <div className="util-box-border-green song-info-group">
                     <img src={artist_icon} />
                     <p>{props.song_artist_title}</p>
                 </div>
-                <div className="song-info-group">
+                <div className="util-box-border-green song-info-group">
                     <img src={genre_icon} />
                     <p>{props.song_genre_title}</p>
                 </div>
