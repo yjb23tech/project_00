72aa40a34637767d7970700565e1feeb7b09a60e
yjb23tech <yjb23tech@gmail.com>
2024-02-05 00:18:02 +0000

    Freeman

--- App.jsx
+++ App.jsx
@@ -1,9 +1,17 @@
 import { createRoot } from "react-dom/client"
+import Card from "./components/Card.jsx"
 
 const App = () => {
     return (
         <div>
             <h1>Lundun Grammar</h1>
+            <Card 
+                song_cover_art = "track_13_song_art.png"
+                song_title = "Benjamins Burn Remix"
+                song_album_title = "My Beautiful Dark Twisted Fantasy"
+                song_artist_title = "Kanye West"
+                song_genre_title = "Baroque Sophisticated Ignorance"
+            />
         </div>
     )
 }
